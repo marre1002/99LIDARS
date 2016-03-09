36533726c4bc4c0a64824535a9d28f06be9b5dc4
Martin <martin_helmersson@hotmail.com>
2016-03-09 16:24:47 +0100

    change in CMakeLists.txt

---
+++ ThreadedPipeline/build/CMakeFiles/thread_filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/thread_filter.dir/ThreadTestFilter.cpp.o"
+  "thread_filter.pdb"
+  "thread_filter"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/thread_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
