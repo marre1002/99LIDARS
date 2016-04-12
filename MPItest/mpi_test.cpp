c766aac8b461316105ef38259c62a67156dbeddf
Martin <martin_helmersson@hotmail.com>
2016-04-12 10:01:25 +0200

    mpi and pcl

--- MPItest/mpi_test.cpp
+++ MPItest/mpi_test.cpp
@@ -1,3 +1,23 @@
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/console/time.h>
+#include <pcl/filters/passthrough.h>
+#include <string>
+
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/features/moment_of_inertia_estimation.h>
+#include <vector>
+#include <pcl/console/parse.h>
+
 #include <iostream>
 #include <mpi.h>
 
@@ -11,16 +31,117 @@ MPI_Init (&argc, &argv);
 MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
 MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
+//************************************************************************************************************
+
+
+if(my_rank == 0){ // I'm master and handle the splitting
+
+  
+  pcl::PCDReader reader;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_main (new pcl::PointCloud<pcl::PointXYZ>);
+
+  reader.read ("../../PCDdataFiles/data02.pcd", *cloud);
+  std::cout << "PointCloud before filtering has: " << cloud->points.size () << " data points." << std::endl; //*
+
+  // Timer object
+  //pcl::console::TicToc tt;
+
+
+  //tt.tic();
+
+
+  pcl::PointCloud<pcl::PointXYZ> cloud_yo;
+
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud0 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud3 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud4 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud5 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud6 (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud7 (new pcl::PointCloud<pcl::PointXYZ>);
+
+  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr > v;
+  v.push_back(cloud0);
+  v.push_back(cloud1);
+  v.push_back(cloud2);
+  v.push_back(cloud3);
+  v.push_back(cloud4);
+  v.push_back(cloud5);
+  v.push_back(cloud6);
+  v.push_back(cloud7);
+
+  // Devide the dataset and keep every n:th point (setting it to 1 will include all points)
+
+  int m_tag = 0; // MPI message tag
+
+  int nth_point = 3;
+  double zero = 0.0000000;
+  for (int iii = 0; iii < static_cast<int> (cloud->size ()); ++iii){ 
+    if((iii%nth_point) == 0){
+    	if(cloud->points[iii].x > zero){
+	          if(cloud->points[iii].y > zero){
+	              if(cloud->points[iii].y > cloud->points[iii].x){
+	                  //cloud0->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	              	  // Send this point to worker one
+	              	  float buff [3] = {cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z};
+	              	  MPI_Send(&buff, 3, MPI_FLOAT, 1, m_tag, MPI_COMM_WORLD);
+	              	  cout << "Just sent values!" ;
+	              }else{
+	                  cloud1->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	              }
+	          }else{
+	              if((abs(cloud->points[iii].y)) > cloud->points[iii].x){
+	                  cloud2->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	              }else{
+	                  cloud3->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	              }
+	          }    
+	      }else{
+	          if(cloud->points[iii].y > zero){
+	              if(cloud->points[iii].y > (abs(cloud->points[iii].x))){
+	                      cloud4->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	                  }else{
+	                      cloud5->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	                  }
+	              }else{
+	                  if(cloud->points[iii].y > cloud->points[iii].x){
+	                      cloud6->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	                  }else{
+	                      cloud7->points.push_back (pcl::PointXYZ (cloud->points[iii].x,cloud->points[iii].y,cloud->points[iii].z));
+	                  }
+	          }
+	      }
+    }else{
+    	//Ignore this point
+    }
+  }
+
+}else if(my_rank == 1){ // Worker1 runs this code
+
+ 	 float buff [3];
+ 	 MPI_Recv(&buff, 3, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+ 	 cout << "received x:" << buff[0] << " y:" << buff[1] << "z:" << buff[2] << endl;
+}
+
+
+
+
+//*****************************************************************************************************************
 // Get the name of the processor
 char processor_name[MPI_MAX_PROCESSOR_NAME];
 int name_len;
 MPI_Get_processor_name(processor_name, &name_len);
 
 // Print off a hello world message
-printf("Hello world from processor %s ", processor_name);
+//printf("Hello world from processor %s ", processor_name);
 
-double time_start = MPI_Wtime();
-std::cout << "Hello World, my rank is " << my_rank <<" "<< MPI_Wtime() - time_start << std::endl;
+//double time_start = MPI_Wtime();
+//std::cout << "Hello World, my rank is " << my_rank <<" "<< MPI_Wtime() - time_start << std::endl;
 // End MPI
 MPI_Finalize ();
 return 0;
