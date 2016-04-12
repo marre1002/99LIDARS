3fe8e01747d339c3bd4cbbb0534080389b006b07
Martin <martin_helmersson@hotmail.com>
2016-04-12 14:44:23 +0200

    mpi update

--- MPItest/mpi_test.cpp
+++ MPItest/mpi_test.cpp
@@ -13,23 +13,32 @@
 #include <pcl/filters/passthrough.h>
 #include <string>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/point_operators.h>
+#include <pcl/common/io.h>
+
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/features/moment_of_inertia_estimation.h>
 #include <vector>
 #include <pcl/console/parse.h>
 
+#include <fstream>
+#include <sstream>
 #include <iostream>
 #include <mpi.h>
+
+using namespace pcl;
+using namespace std;
 
 static int numprocs;
 
 int main(int argc, char **argv) {
-int my_rank;
+int my_rank = 0;
 // MPI initializations
-MPI_Status status;
-MPI_Init (&argc, &argv);
-MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
-MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+//MPI_Status status;
+//MPI_Init (&argc, &argv);
+//MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+//MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
 //************************************************************************************************************
 
@@ -39,19 +48,83 @@ if(my_rank == 0){ // I'm master and handle the splitting
   // Timer object
   pcl::console::TicToc tt;
   
-  pcl::PCDReader reader;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_main (new pcl::PointCloud<pcl::PointXYZ>);
+  //pcl::PCDReader reader;
+  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_main (new pcl::PointCloud<pcl::PointXYZ>);
 
-  reader.read ("../../PCDdataFiles/data02.pcd", *cloud);
-  std::cout << "PointCloud before filtering has: " << cloud->points.size () << " data points." << std::endl; //*
+  //reader.read ("../../PCDdataFiles/data02.pcd", *cloud);
+  //std::cout << "PointCloud before filtering has: " << cloud->points.size () << " data points." << std::endl; //*
+
+    tt.tic();
+
+	std::string infile = "../../PCDdataFiles/002.bin";
+
+	// load point cloud
+	fstream input(infile.c_str(), ios::in | ios::binary);
+	if(!input.good()){
+		cerr << "Could not read file: " << infile << endl;
+		exit(EXIT_FAILURE);
+	}
+	input.seekg(0, ios::beg);
+
+	pcl::PointCloud<PointXYZ>::Ptr points (new pcl::PointCloud<PointXYZ>);
+
+	float ignore;
+	int i;
+	for (i=0; input.good() && !input.eof(); i++) {
+		PointXYZ point;
+		input.read((char *) &point.x, 3*sizeof(float));
+		input.read((char *) &ignore, sizeof(float));
+		points->push_back(point);
+	}
+	input.close();
+
+	cout << "Read KTTI point cloud with " << i << " points in " << tt.toc() << " ms." << endl;
+
+  /*float buffer [500000];
+  
+  std::ifstream infile("../../PCDdataFiles/data02.pcd");
+  std::string line;
+  int points = 0;
+  int count = -1;
+  float x, y, z, i;
+  while (std::getline(infile, line)){
+	    std::istringstream iss(line);
+	    
+	    if ((iss >> x >> y >> z >> i)) { 
+	    	buffer[++count];
+	    	buffer[++count];
+	    	buffer[++count];
+	    }
+	    // process pair (a,b)
+	}
+	count++;
+
+	cout << "done reading the file, points: " << count << " in " << tt.toc() << " ms." << endl;	
+/*
+
+for(std::string line; std::getline(source, line); )   //read stream line by line
+{
+    std::istringstream in(line);      //make a stream for the line itself
+
+    std::string type;
+    in >> type;                  //and read the first whitespace-separated token
+
+    if(type == "triangle")       //and check its value
+    {
+        float x, y, z;
+        in >> x >> y >> z;       //now read the whitespace-separated floats
+    }
+    else if(...)
+        ...
+    else
+        ...
+}
+*/
 
 
-
-
-
-  pcl::PointCloud<pcl::PointXYZ> cloud_yo;
+  //pcl::PointCloud<pcl::PointXYZ> cloud_yo;
 
 
   tt.tic();
@@ -66,10 +139,10 @@ if(my_rank == 0){ // I'm master and handle the splitting
   float gg [20000];
   float hh [20000];
   int count0,count1,count2,count3,count4,count5,count6,count7 = -1;
-
+/*
   int nth_point = 3;
   double zero = 0.0000000;
-  for (int iii = 0; iii < static_cast<int> (cloud->size ()); ++iii){ 
+  for (int iii = 0; iii < 10; ++iii){ 
     if((iii%nth_point) == 0){
     	if(cloud->points[iii].x > zero){
 	          if(cloud->points[iii].y > zero){
@@ -130,7 +203,7 @@ if(my_rank == 0){ // I'm master and handle the splitting
    // Send the float buffer (first sector)
    MPI_Send(&aa, count0, MPI_FLOAT, 1, m_tag, MPI_COMM_WORLD);
 
-   cout << "Sending data in: " << tt.toc() << " ms" << endl;
+   cout << "Sending data in: " << tt.toc() << " ms" << endl; */
    
 
 }else if(my_rank == 1){ // Worker1 runs this code
@@ -161,17 +234,8 @@ if(my_rank == 0){ // I'm master and handle the splitting
 
 
 //*****************************************************************************************************************
-// Get the name of the processor
-char processor_name[MPI_MAX_PROCESSOR_NAME];
-int name_len;
-MPI_Get_processor_name(processor_name, &name_len);
 
-// Print off a hello world message
-//printf("Hello world from processor %s ", processor_name);
-
-//double time_start = MPI_Wtime();
-//std::cout << "Hello World, my rank is " << my_rank <<" "<< MPI_Wtime() - time_start << std::endl;
 // End MPI
-MPI_Finalize ();
+//MPI_Finalize ();
 return 0;
 }
