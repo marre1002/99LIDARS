c8195b9142a355a6a12c8722496410c3c184fcce
Martin <martin_helmersson@hotmail.com>
2016-04-12 09:07:58 +0200

    update to mpitest

--- MPItest/mpi_test.cpp
+++ MPItest/mpi_test.cpp
@@ -10,6 +10,16 @@ MPI_Status status;
 MPI_Init (&argc, &argv);
 MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
 MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+
+// Get the name of the processor
+char processor_name[MPI_MAX_PROCESSOR_NAME];
+int name_len;
+MPI_Get_processor_name(processor_name, &name_len);
+
+// Print off a hello world message
+printf("Hello world from processor %s, rank %d out of %d processors\n",
+         processor_name, world_rank, world_size);
+
 double time_start = MPI_Wtime();
 std::cout << "Hello World, my rank is " << my_rank <<" "<< MPI_Wtime() - time_start << std::endl;
 // End MPI
