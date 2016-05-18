15b28076f92620dd4d756e506121e9c59188bb02
Lucas <eldritch116@gmail.com>
2016-05-18 13:26:03 +0200

    Test

--- MPItest/mpi_test.cpp
+++ MPItest/mpi_test.cpp
@@ -86,14 +86,14 @@ int main(int argc, char **argv) {
 
 	// MPI initializations
 
-	std::cout << "Hue" << endl;
+	
 
 	MPI_Status status;
 	MPI_Init (&argc, &argv);
 	MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
 	MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
-
+	std::cout << "Hue" << endl;
 /*******************************************************************************************
 *		Master runs this code
 ********************************************************************************************/
