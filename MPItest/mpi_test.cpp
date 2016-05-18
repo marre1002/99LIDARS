7191399f1a24f599bacaddb9467beb33e53acd38
Lucas <eldritch116@gmail.com>
2016-05-18 13:30:31 +0200

    Testing MPI

--- MPItest/mpi_test.cpp
+++ MPItest/mpi_test.cpp
@@ -86,14 +86,14 @@ int main(int argc, char **argv) {
 
 	// MPI initializations
 
-	
+
 
 	MPI_Status status;
 	MPI_Init (&argc, &argv);
 	MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
 	MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
-	std::cout << "Hue" << endl;
+	
 /*******************************************************************************************
 *		Master runs this code
 ********************************************************************************************/
@@ -154,6 +154,8 @@ int main(int argc, char **argv) {
 			objects.push_back(c);
 
 		}
+
+		std::cout << "I have collected CLUSTERS!" << endl;
 
 		clusterCount = clusterCount + (number_amount/3);
 		free(number_buf);	
